571fb7f3a70756ac2e2f30da9a161823167521d7
mdm106 <mads.mcmahon@googlemail.com>
2020-07-02 14:41:11 +0100

    Header component created and added to app.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+    <div className="header-container">
+        <header className="header-main">
+            <h1>Maddy McMahon</h1>
+            <h5>Junior Web Developer</h5>
+        </header>
+    </div>
+);
+
+export default Header;
