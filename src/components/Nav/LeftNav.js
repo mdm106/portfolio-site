b5df7633b60cc4a068e52d5f131a8e49ac919e3c
mdm106 <mads.mcmahon@googlemail.com>
2020-07-04 14:29:48 +0100

    Styling of left nav bar

--- src/components/Nav/LeftNav.js
+++ src/components/Nav/LeftNav.js
@@ -7,7 +7,7 @@ const StyledUl = styled.ul`
     list-style: none;
     display: flex;
     flex-flow: column nowrap;
-    background-color: white;
+    background-color: rgb(45,49,52);
     position: fixed;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
     top: 0;
@@ -16,10 +16,18 @@ const StyledUl = styled.ul`
     width: 55vw;
     transition: transform 0.3s ease-in-out;
     margin: 0;
+    padding-top: 4rem;
     z-index: 20;
 
-    li {
-        color: black;
+    li h6 {
+        color: white;
+        font-family: var(--montserrat);
+        font-size: 1rem;
+        margin: 1.2rem 0;
+    }
+
+    a {
+        text-decoration: none;
     }
 `;
 
