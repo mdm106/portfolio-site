e3d8b83c099c7cb5dd0e2b79b76e4be33b2e5124
mdm106 <mads.mcmahon@googlemail.com>
2020-07-04 15:12:59 +0100

    Skills component populated and styling added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ import NavItem from "./Nav/NavItem";
 import Burger from "./Nav/Burger";
 
 const Skills = () => (
-    <div>
+    <>
         <Media queries={{ mobile: { maxWidth: 699 } }}>
             {matches => 
                 matches.mobile ? (
@@ -32,7 +32,29 @@ const Skills = () => (
                     )
                 }
         </Media>
-    </div>
+        <div className="container page-content">
+            <div className="icon-wrapper">
+        
+                    <i className="fas fa-sitemap fa-5x"></i>
+         
+                    <i className="fas fa-laptop-code fa-5x"></i>
+       
+                    <i className="far fa-keyboard fa-5x"></i>
+            </div>
+                <section className="main-text-skills">
+                <p>HTML / CSS / SASS / Responsive design / Bootstrap</p>
+                <p>Javascript  / React / Redux / Axios</p>
+                
+                <p>Object Oriented programming / Functional programming</p>
+                
+                <p>Git / GitHub / Gulp / npm / Composer</p>
+                    
+                <p>PHP / Laravel / MySQL / Guzzle HTTP</p>
+                
+                <p>SQL / R</p>
+                </section>
+        </div>
+    </>
     
 );
 
