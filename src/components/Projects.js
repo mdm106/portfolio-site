a68caca65e6b19b68860df991c7c695253f015b2
mdm106 <mads.mcmahon@googlemail.com>
2020-07-04 11:26:15 +0100

    Burger menu added to projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,38 @@
 import React from "react";
+import Media from 'react-media';
 
 import Nav from "./Nav/Nav";
 import NavItem from "./Nav/NavItem";
+import Burger from './Nav/Burger';
 
 const Projects = () => (
-    <Nav>
-        <NavItem classID="no-highlight"
-                  content="Home"
-                  link="/" />
-        <NavItem classID="no-highlight"
-                  content="About"
-                  link="/about" />
-        <NavItem classID="no-highlight"
-                  content="Skills"
-                  link="/skills" /> 
-        <NavItem classID="highlight"
-                  content="Projects"
-                  link="/projects" />  
-        <NavItem classID="no-highlight"
-                  content="Contact"
-                  link="/contact" />   
-    </Nav>
+    <div>
+        <Media queries={{ mobile: { maxWidth: 699 } }}>
+            {matches => 
+                matches.mobile ? (
+                    <Burger />
+                ) : (
+                    <Nav>
+                        <NavItem classID="no-highlight"
+                                content="Home"
+                                link="/" />
+                        <NavItem classID="no-highlight"
+                                content="About"
+                                link="/about" />
+                        <NavItem classID="no-highlight"
+                                content="Skills"
+                                link="/skills" /> 
+                        <NavItem classID="highlight"
+                                content="Projects"
+                                link="/projects" />  
+                        <NavItem classID="no-highlight"
+                                content="Contact"
+                                link="/contact" />   
+                    </Nav>
+                   )
+                }
+        </Media>
+    </div> 
 );
 
 export default Projects;
