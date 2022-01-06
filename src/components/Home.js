55d50c194ccc3149927ebd7fec9a39b350f05921
mdm106 <mads.mcmahon@googlemail.com>
2022-01-06 20:43:02 +0000

    Text amends

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,9 +34,9 @@ const Home = () => (
             <div className="container page-content">
                 <img className="main-image" src={headshot} alt="Headshot of Maddy McMahon"></img>
                 <section className="main-text">
-                    <p>I am a junior web developer, working in an full-stack role since August 2020. I am a driven and conscientious individual, with a keen eye for detail. I love using my logic skills to solve programming problems, and I am persistent when faced with challenging questions. I also have an affinity with data, having previously worked as a data analyst for several years.
+                    <p>I am a junior web developer, working in a full-stack role since August 2020. I am a driven and conscientious individual, with a keen eye for detail. I love using my logic skills to solve programming problems, and I am persistent when faced with challenging questions. I also have an affinity with data, having previously worked as a data analyst for several years.
                     </p>
-                    <p>My commercial experience has primarily been in Ruby on Rails applications. I also have used Vue.js, jQuery and Angular in a commercial setting. Prior to my first commercial role, I completed a 12 week bootcamp with DevelopMe where I learnt Laravel and React.
+                    <p>My commercial experience has primarily been in Ruby on Rails applications. I have also used Vue.js, jQuery and Angular in a commercial setting. Prior to my first web development role, I completed a 12 week bootcamp with DevelopMe where I learnt Laravel and React.
                     </p>
                     <p>In my spare time I enjoy hiking, with the Lake District and the Alps as personal favourite spots. I am also a huge fan of watching live sport, especially football and cricket.
                     </p>
